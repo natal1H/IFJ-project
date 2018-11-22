aa6b5070611736a1799690e2f1b488b304bf7310
Natalia Holkova <xholko02@stud.fit.vutbr.cz>
2018-11-22 18:30:31 +0100

    Generovanie inštrukcií prevodu

--- code_gen.c
+++ code_gen.c
@@ -36,7 +36,7 @@ char *get_string_with_prefix(char *str, char *prefix) {
     return str_complete;
 }
 
-int add_aritmetic_instruction(tInstruction_type type, char *var_name, char *symbol1, char *symbol2, char *symbol_prefix, bool global) {
+int add_instruction_with_2_symbols(tInstruction_type type, char *var_name, char *symbol1, char *symbol2, char *symbol1_prefix, char *symbol2_prefix, bool global) {
     if (var_name == NULL || symbol1 == NULL || symbol2 == NULL) {
         // Chyba
         return -1; // TODO nájsť vhodný chybový kód
@@ -44,8 +44,8 @@ int add_aritmetic_instruction(tInstruction_type type, char *var_name, char *symb
 
     // Pred var a pred symboly dať predponu
     char *var_complete = get_string_with_prefix(var_name, (global ? "GF@" : "LF@") );
-    char *symbol1_complete = get_string_with_prefix(symbol1, symbol_prefix );
-    char *symbol2_complete = get_string_with_prefix(symbol2, symbol_prefix );
+    char *symbol1_complete = get_string_with_prefix(symbol1, symbol1_prefix );
+    char *symbol2_complete = get_string_with_prefix(symbol2, symbol2_prefix );
     if (var_complete == NULL || symbol1_complete == NULL || symbol2_complete == NULL) {
         // Chyba pri alokácii
         return -1;
@@ -65,38 +65,83 @@ int add_aritmetic_instruction(tInstruction_type type, char *var_name, char *symb
     return ERR_OK;
 }
 
+int add_instruction_with_1_symbol(tInstruction_type type, char *var_name, char *symbol, char *symbol_prefix, bool global) {
+    if (var_name == NULL || symbol == NULL) {
+        // Chyba
+        return -1; // TODO nájsť vhodný chybový kód
+    }
+
+    // Pred var a pred symboly dať predponu
+    char *var_complete = get_string_with_prefix(var_name, (global ? "GF@" : "LF@") );
+    char *symbol_complete = get_string_with_prefix(symbol, symbol_prefix );
+    if (var_complete == NULL || symbol_complete == NULL) {
+        // Chyba pri alokácii
+        return -1;
+    }
+
+    // Nastaviť správne aktuálnu inštrukciu
+    tInstr_set_instruction(curr_instr, type, var_complete, symbol_complete, NULL);
+
+    // Vložiť inštrukciu do zoznamu
+    listInsertPostActive(&instr_list, curr_instr);
+
+    // Uvoľniť miesto, kde boli var_complete, symbol1_complete, symbol2_complete
+    free(var_complete);
+    free(symbol_complete);
+
+    return ERR_OK;
+
+}
+
 int gen_add_int(char *var_name, char *symbol1, char *symbol2, bool global) {
-    return add_aritmetic_instruction(I_ADD, var_name, symbol1, symbol2, "int@", global);
+    return add_instruction_with_2_symbols(I_ADD, var_name, symbol1, symbol2, "int@", "int@", global);
 }
 
 int gen_add_float(char *var_name, char *symbol1, char *symbol2, bool global) {
-    return add_aritmetic_instruction(I_ADD, var_name, symbol1, symbol2, "float@", global);
+    return add_instruction_with_2_symbols(I_ADD, var_name, symbol1, symbol2, "float@", "float@", global);
 }
 
 int gen_sub_int(char *var_name, char *symbol1, char *symbol2, bool global) {
-    return add_aritmetic_instruction(I_SUB, var_name, symbol1, symbol2, "int@", global);
+    return add_instruction_with_2_symbols(I_SUB, var_name, symbol1, symbol2, "int@", "int@", global);
 }
 
 int gen_sub_float(char *var_name, char *symbol1, char *symbol2, bool global) {
-    return add_aritmetic_instruction(I_SUB, var_name, symbol1, symbol2, "float@", global);
+    return add_instruction_with_2_symbols(I_SUB, var_name, symbol1, symbol2, "float@", "float@", global);
 }
 
 int gen_mul_int(char *var_name, char *symbol1, char *symbol2, bool global) {
-    return add_aritmetic_instruction(I_MUL, var_name, symbol1, symbol2, "int@", global);
+    return add_instruction_with_2_symbols(I_MUL, var_name, symbol1, symbol2, "int@", "int@", global);
 }
 
 int gen_mul_float(char *var_name, char *symbol1, char *symbol2, bool global) {
-    return add_aritmetic_instruction(I_MUL, var_name, symbol1, symbol2, "float&", global);
+    return add_instruction_with_2_symbols(I_MUL, var_name, symbol1, symbol2, "float@", "float@", global);
 }
 
 int gen_div(char *var_name, char *symbol1, char *symbol2, bool global) {
-    return add_aritmetic_instruction(I_DIV, var_name, symbol1, symbol2, "float@", global);
+    return add_instruction_with_2_symbols(I_DIV, var_name, symbol1, symbol2, "float@", "float@", global);
 }
 int gen_idiv(char *var_name, char *symbol1, char *symbol2, bool global) {
-    return  add_aritmetic_instruction(I_IDIV, var_name, symbol1, symbol2, "int@", global);
+    return add_instruction_with_2_symbols(I_IDIV, var_name, symbol1, symbol2, "int@", "int@", global);
+}
+
+int gen_int2float(char *var_name, char *symbol, bool global) {
+    return add_instruction_with_1_symbol(I_INT2FLOAT, var_name, symbol, "int@", global);
+}
+
+int gen_float2int(char *var_name, char *symbol, bool global) {
+    return add_instruction_with_1_symbol(I_FLOAT2INT, var_name, symbol, "float@", global);
+}
+
+int gen_int2char(char *var_name, char *symbol, bool global) {
+    return add_instruction_with_1_symbol(I_INT2CHAR, var_name, symbol, "int@", global);
+}
+
+int gen_stri2int(char *var_name, char *symbol1, char *symbol2, bool global) {
+    return add_instruction_with_2_symbols(I_STRI2INT, var_name, symbol1, symbol2, "string@", "int@", global);
 }
 
 // test
+/*
 int main() {
     printf("CODE GEN TEST\n");
 
@@ -115,9 +160,15 @@ int main() {
     gen_div("sub_var", "2.5", "1.2", true);
     gen_idiv("sub_var", "2.5", "1.2", true);
 
+    gen_int2float("myvar", "5", false);
+    gen_float2int("myvar2", "5.2", true);
+    gen_int2char("myvar2", "64", true);
+    gen_stri2int("str", "text", "2", true);
+
     list_print_instructions(&instr_list);
 
     code_gen_end();
 
     return 0;
 }
+ */
--- code_gen.h
+++ code_gen.h
@@ -22,6 +22,7 @@
     int code_gen_end();
 
     // Pomocné funkcie pre generovanie rôznych situácií
+    // Aritmetické inštrukcie
     int gen_add_int(char *var_name, char *symbol1, char *symbol2, bool global);
     int gen_add_float(char *var_name, char *symbol1, char *symbol2, bool global);
     int gen_sub_int(char *var_name, char *symbol1, char *symbol2, bool global);
@@ -30,9 +31,16 @@
     int gen_mul_float(char *var_name, char *symbol1, char *symbol2, bool global);
     int gen_div(char *var_name, char *symbol1, char *symbol2, bool global);
     int gen_idiv(char *var_name, char *symbol1, char *symbol2, bool global);
+    // Inštrukcie na prevody
+    int gen_int2float(char *var_name, char *symbol, bool global);
+    int gen_float2int(char *var_name, char *symbol, bool global);
+    int gen_int2char(char *var_name, char *symbol, bool global);
+    int gen_stri2int(char *var_name, char *symbol1, char *symbol2, bool global);
 
     // Ďalšie pomocné funkcie
     char *get_string_with_prefix(char *str, char *prefix);
-    int add_aritmetic_instruction(tInstruction_type type, char *var_name, char *symbol1, char *symbol2, char *symbol_prefix, bool global);
+    int add_instruction_with_2_symbols(tInstruction_type type, char *var_name, char *symbol1, char *symbol2, char *symbol1_prefix, char *symbol2_prefix, bool global);
+    int add_instruction_with_1_symbol(tInstruction_type type, char *var_name, char *symbol, char *symbol_prefix, bool global);
+
 
 #endif
