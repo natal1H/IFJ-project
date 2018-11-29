1c011067580f7af3b050637d4733457ce4aeaf20
Filip Bali <14928466+filipbali@users.noreply.github.com>
2018-11-29 19:57:20 +0100

    Ak sa nepodari realokacia potom vrat ERR_INTERNAL

--- parser.c
+++ parser.c
@@ -324,9 +324,12 @@ printf("%s ", token->attribute);
 
 		// Potrebujem zálohovať ID, lebo budem brať ďalšie tokeny
 		id_copy = (char *) realloc(id_copy, sizeof(char) * strlen(token->attribute));
+		if(id_copy == NULL){ //Nepodarila sa realokacia
+			return ERR_INTERNAL;
+		}
 		strcpy(id_copy, token->attribute);
 printf("\n##ID COPY: %s\n", id_copy);
-		
+
 		GET_NEXT_TOKEN();
 
 		withoutBrackets = false;
@@ -643,6 +646,9 @@ printf("\nExpected number params: %d\n", expected_params);
 
 			// Potrebujem zálohovať ID, lebo budem brať ďalšie tokeny
 			func_id_copy = (char *) realloc(id_copy, sizeof(char) * strlen(token->attribute));
+			if(func_id_copy == NULL){ //Nepodarila sa realokacia
+				return ERR_INTERNAL;
+			}
 			strcpy(func_id_copy, token->attribute);
             
             GET_NEXT_TOKEN();
